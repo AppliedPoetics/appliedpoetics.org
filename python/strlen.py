0f5ce02b5eb0d2d57741b5c8cc3696b073e02891
Douglas Luman <admin@appliedpoetics.org>
2016-02-20 20:04:29 -0500

    Missed commenting on strlen.py, the 'At Length' routine.

--- python/strlen.py
+++ python/strlen.py
@@ -3,17 +3,21 @@
 import getopt, re, string, sys, ap_encoding
 
 def bylen(text,length):
+	#sanitize punctuation
 	exclude = set(string.punctuation)
 	text = ''.join(ch for ch in text if ch not in exclude)
+	#explode text into word array
 	words = text.split()
+	#initialize container
 	rightSize = []
+	#loop through text to select words matching length
 	for x in range(0,len(words)):
 		if len(words[x]) == int(length):
 			rightSize.append(words[x])
 	return rightSize
 
 def main(argv):
-        input_text = ''
+        #get passed arguments, where "text" is the path to scratch/, "length" is the length of word being searched
         try:
                 opts,args = getopt.getopt(argv,"t:l:",["text=","length="])
         except getopt.GetoptError:
@@ -28,6 +32,7 @@ def main(argv):
 			length = arg
                 else:
                         sys.exit(2)
+	#read text from file
 	text = ap_encoding.read_file(text)
         print ', '.join(bylen(text,length)) 
 

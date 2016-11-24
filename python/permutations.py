1286c9b81dc77f540a1d4d958c661bfa9951b77d
Douglas Luman <admin@appliedpoetics.org>
2016-11-24 12:20:06 -0500

    Anagrams work even better and faster (2 word combinations only).

--- python/permutations.py
+++ python/permutations.py
@@ -8,23 +8,31 @@ def remove_letters(s1,s2):
 def get_anagrams(s):
 	return [word for word in anagram.anagram(s)]
 
+def set_inventory(s):
+	inventory = dict([(l,0) for l in s])
+	for l in s: inventory[l] += 1
+	return inventory
+
+def test_phrase(phrase,anagrams,color):
+	phrases = []
+	for anagram in anagrams:
+		inventory = set_inventory(color)
+		test = phrase + ' ' + anagram
+		for l in test.replace(' ',''): inventory[l] -= 1
+		total = sum(inventory.itervalues())
+		if total == 0: phrases.append(test)
+	return phrases
+
 def all_anagrams(color):
 	phrases = []
-	anagrams = get_anagrams(color)
-
+	nospace = color.replace(' ','')
+	anagrams = get_anagrams(nospace)
+		
 	for gram in anagrams:
 		is_anagram = True
-		remainder = remove_letters(gram,color)
-		phrase = gram
-		while is_anagram:
-			nagaram = get_anagrams(remainder)
-			if len(nagaram) > 0:
-				for word in nagaram:
-					if len(phrase) >= len(color): pass
-					else: phrase = phrase + " " + word
-					remainder = remove_letters(word,remainder)
-			else:
-				is_anagram = False
-		if len(phrase.replace(' ','')) >= len(color.replace(' ','')): phrases.append(phrase)
-
-	return '\n'.join(phrases)
+		remainder = remove_letters(gram,nospace)
+		nagarams = get_anagrams(remainder)
+		results = test_phrase(gram,nagarams,nospace)
+		for result in results: phrases.append(result)
+	if len(phrases) == 0: phrases.append(color)
+	return ', '.join(phrases)

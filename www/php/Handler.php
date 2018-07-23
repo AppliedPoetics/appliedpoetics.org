262d492b921f214ab8f73abc4f0d33db7e702c57
Douglas Luman <admin@appliedpoetics.org>
2018-07-23 06:27:31 -0400

    Fixing a small issue with debug call.

--- www/php/Handler.php
+++ www/php/Handler.php
@@ -1,6 +1,6 @@
 <?php
     require('../../php/Config.php');
-    if(defined(IS_DEBUG) AND IS_DEBUG){
+    if(defined('IS_DEBUG') AND IS_DEBUG){
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);

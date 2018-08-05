38ffb327f114970c4e01ab042ad1db217428fc51
Douglas Luman <admin@appliedpoetics.org>
2018-08-05 09:10:15 -0400

    Updating progress.

--- julia/anagram.jl
+++ julia/anagram.jl
@@ -1,6 +1,7 @@
 #!/usr/bin/julia
 
 include("Main.jl")
+using DataStructures
 using Controller
 using Common
 
@@ -9,19 +10,46 @@ type Word
     index
 end
 
-function sort_words(words)
-    return [i for (i,a) in enumerate(words)]
-end
-
-function get_words(t)
-        return [Word(w,find(t .== w)) for w in t]
-end
-
-function nagaram(t,n)
-    words = get_words(t)
-    for i=1:n
-        words[i] = sort(words[i].string,by=sort_words)
+function counter(w)
+    counter = Dict()
+    for char in Set(w)
+        pattern = Regex("[$char]","ism")
+        counter[char] = length(matchall(pattern,w))
     end
+    return counter
+end
+
+function size(t)
+    result = Dict()
+    for w in t
+        key = length(w)
+        try
+            push!(result["$key"],w)
+        catch e
+            result["$key"] = [w]
+        end
+    end
+    return result
+end
+
+function compare(w,m)
+    is_match = false
+    if m == w
+        return false
+    end
+    _w, _m = counter(w), counter(m)
+    if keys(_w) == keys(_m)
+        is_match = _w == _m
+    end
+    return is_match
+end
+
+function nagaram(t)
+    results = Dict()
+    words = size(Set(t))
+    #ITERATE OVER WORDS GROUPED BY LENGTH
+    #IN SOME EFFICIENT WAY.
+    return results
 end
 
 function main()
@@ -29,7 +57,7 @@ function main()
     t = Controller.read_file(args["t"])
     t = Common.depunctuate(t)
     t = Common.split_to_lower(t)
-    print(nagaram(t,length(t)))
+    println(nagaram(t))
 end
 
 main()

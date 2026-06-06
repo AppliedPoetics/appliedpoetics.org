82dab871785383bd9b9cc98dad4747fbdfc77181
dluman <dluman@douglasjluman.com>
2026-06-06 10:40:05 -0400

    Adding OpenGraph and Twitter data

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://appliedpoetics.org',
   devToolbar: {
     enabled: false
   },
